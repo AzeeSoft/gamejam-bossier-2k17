4fc47871d5ace3bfc7c1c456170a69181d1c87a7
DEIMOS\Mackenzy <mackenzy.green95>
2017-10-14 12:06:41 -0500

    Menu music no longer plays in game, it is set to inactive for in game

---
+++ Assets/Scripts/MenuScripts/DisplayControls.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayControls : MonoBehaviour {
+
+
+    public void Awake()
+    {
+            GameObject[] objs = GameObject.FindGameObjectsWithTag("menuMusic");
+            if (objs.Length > 1)
+                Destroy(this.gameObject);
+            DontDestroyOnLoad(this.gameObject);
+    }
+}
--- Assets/Scripts/MenuScripts/sceneLoad.cs
+++ Assets/Scripts/MenuScripts/sceneLoad.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class sceneLoad : MonoBehaviour {
+    private GameObject[] sound;
 
-	public void LoadScene(int level)
+    public void LoadScene(int level)
     {
+        sound= GameObject.FindGameObjectsWithTag("menuMusic");
+        if (level == 2)
+            sound[0].SetActive(false);
         Application.LoadLevel(level);
     }
 }
