bd616bb85ab515de7c00d8733161f02b35a6f043
Azee <aziztitu1996@gmail.com>
2017-10-13 22:09:47 -0500

    Added Environment, Character Movement, and Basic Jump action

---
+++ Assets/Scripts/Pecky/PeckyMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeckyMovement : MonoBehaviour
+{
+    public float playerSpeed;
+    public float jumpForce;
+
+    public bool facingRight = true;
+
+    Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkForJump();
+        updateMovement();
+    }
+
+    void checkForJump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(Vector2.up*jumpForce);
+        }
+    }
+
+    void updateMovement()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        if ((x < 0 && facingRight) || (x > 0 && !facingRight))
+        {
+            facingRight = !facingRight;
+            Vector3 scale = StaticTools.CloneVector3(transform.localScale);
+            scale.x *= -1;
+            transform.localScale = scale;
+        }
+
+        Vector3 pos = StaticTools.CloneVector3(transform.localPosition);
+        pos.x += x * playerSpeed;
+        transform.localPosition = pos;
+    }
+}
---
+++ Assets/Scripts/StaticTools.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+/// <summary>
+/// Common methods used in different places in the game.
+/// </summary>
+class StaticTools
+{
+    /// <summary>
+    /// Remaps a value from one range to another.
+    /// </summary>
+    /// <param name="value">The value you want to remap</param>
+    /// <param name="from1">Starting value of the actual range</param>
+    /// <param name="to1">Ending value of the actual range</param>
+    /// <param name="from2">Starting value of the new range</param>
+    /// <param name="to2">Starting value of the new range</param>
+    /// <returns>The value remapped in the new range</returns>
+    public static float Remap(float value, float from1, float to1, float from2, float to2)
+    {
+        return (value - from1) / (to1 - from1) * (to2 - from2) + from2;
+    }
+
+    /// <summary>
+    /// Deep copies a Vector3 object
+    /// </summary>
+    /// <param name="v">Actual Vector3 object</param>
+    /// <returns>Deep-copied Vector3 obbject</returns>
+    public static Vector3 CloneVector3(Vector3 v)
+    {
+        Vector3 newVector3 = new Vector3(v.x, v.y, v.z);
+        return newVector3;
+    }
+
+    /// <summary>
+    /// Deep copies a Vector2 object
+    /// </summary>
+    /// <param name="v">Actual Vector2 object</param>
+    /// <returns>Deep-copied Vector2 obbject</returns>
+    public static Vector3 CloneVector2(Vector2 v)
+    {
+        Vector2 newVector2 = new Vector2(v.x, v.y);
+        return newVector2;
+    }
+
+    /// <summary>
+    /// Deep copies a Quaternion object
+    /// </summary>
+    /// <param name="q">Actual Quaternion object</param>
+    /// <returns>Deep-copied Quaternion object</returns>
+    public static Quaternion CloneQuaternion(Quaternion q)
+    {
+        Vector3 eulerAngles = q.eulerAngles;
+        Quaternion newQuaternion = Quaternion.Euler(eulerAngles.x, eulerAngles.y, eulerAngles.z);
+        return newQuaternion;
+    }
+
+    /// <summary>
+    /// Returns the time passed since game started in milliseconds.
+    /// </summary>
+    /// <returns>The time passed since game started in milliseconds.</returns>
+    public static long GetCurrentTimeMillis()
+    {
+        return (long)(Time.time*1000);
+    }
+}
