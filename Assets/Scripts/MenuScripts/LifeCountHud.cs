5080975de3d88b30fa418714e644da847a226396
DEIMOS\Mackenzy <mackenzy.green95>
2017-10-14 16:05:25 -0500

    added music and started working on hub

---
+++ Assets/Scripts/MenuScripts/LifeCountHud.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeCountHud : MonoBehaviour {
+
+    private int numberLives;
+    private Text lifeValue;
+
+    public LifeCountHud()
+    {
+        numberLives = 3;
+        lifeValue = GetComponent<Text>();
+        lifeValue.text = "" + numberLives + " X";
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
