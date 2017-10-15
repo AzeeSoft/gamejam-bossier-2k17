3b9572632ebb510f85aac4be3a3dc87a6292be5b
DEIMOS\Mackenzy <mackenzy.green95>
2017-10-14 23:19:38 -0500

    added splash and fixed all buttons

---
+++ Assets/Scripts/MenuScripts/SplashThing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplashThing : MonoBehaviour {
+
+    public float delayTime = 5;
+    IEnumerator Start()
+    {
+        yield return new WaitForSeconds(delayTime);
+        Application.LoadLevel(1);
+    }
+}
