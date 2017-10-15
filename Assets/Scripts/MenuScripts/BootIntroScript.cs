430b02d30ee3d2cc08a0d416ab04e55e1a435261
Trenton Robichaux <trobichaux1998@gmail.com>
2017-10-15 11:25:13 -0500

    Fixed boot screen

--- Assets/Scripts/MenuScripts/BootIntroScript.cs
+++ Assets/Scripts/MenuScripts/BootIntroScript.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BootIntroScript : MonoBehaviour {
-
+    public float delayTime = 5.76f;
+    public Canvas BootCanvas;
+    public GameObject BootTextPrefab;
+    private GameObject BootTextObject;
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+	IEnumerator Start () {
+        Vector3 oldTransform = BootTextPrefab.transform.position;
+        BootTextPrefab.transform.position = new Vector3(1000000, 1000000, 0);
+        yield return new WaitForSeconds(delayTime);
+        BootTextPrefab.transform.position = oldTransform;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
         if (Input.GetKeyDown(KeyCode.Return))
         {
             Debug.Log("Enter was pressed...");
